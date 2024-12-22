bb7b658230356eb4c834fc8b9e5001038b6a6107
Alir3za <alireza2212kazemi@gmail.com>
2024-12-22 13:03:35 +0330

    handling fetch errors

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,24 +3,32 @@ import BlogsList from "./BlogsList";
 
 const Home = () => {
 	let [blogs, setBlog] = useState(null);
+	let [name, setName] = useState("ali");
+	let [isLoading, setIsLoading] = useState(true);
+	let [error, setError] = useState(null);
 
 	const handleDelete = (id) => {
 		const newBlogs = blogs.filter((blog) => blog.id !== id);
 		setBlog(newBlogs);
 	};
 
-	let [name, setName] = useState("ali");
-	let [isLoading, setIsLoading] = useState(true);
-
 	useEffect(() => {
-		setInterval(() => {
-			//here for just simulating loading, we just used set interval
+		setTimeout(() => {
+			//here for just simulating loading, we just used set timeout
 			fetch("http://localhost:8000/blogs")
 				.then((res) => {
+					if (!res.ok) {
+						throw new Error("something wrong with fetching");
+					}
 					return res.json();
 				})
 				.then((data) => {
+					setError(null);
 					setBlog(data);
+					setIsLoading(false);
+				})
+				.catch((err) => {
+					setError(err.message);
 					setIsLoading(false);
 				});
 		}, 2000);
@@ -36,6 +44,7 @@ const Home = () => {
 
 	return (
 		<div className="Home">
+			{error && <p>{error}</p>}
 			{isLoading && <div>Loading ...</div>}
 			{blogs && (
 				<BlogsList
@@ -44,14 +53,16 @@ const Home = () => {
 					handleDelete={handleDelete}
 				/>
 			)}
-			<button
-				onClick={() => {
-					changeName();
-				}}
-			>
-				change name
-			</button>
-			<p>{name}</p>
+			<div className="name-button-container">
+				<button
+					onClick={() => {
+						changeName();
+					}}
+				>
+					change name
+				</button>
+				<p>{name}</p>
+			</div>
 			{blogs && (
 				<BlogsList
 					blogs={blogs.filter((blog) => blog.author === "alireza")}
