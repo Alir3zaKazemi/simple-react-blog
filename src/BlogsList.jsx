d1dd59da6c88ec22cd08e37179c0b9d7fee0f3e1
Alir3za <alireza2212kazemi@gmail.com>
2024-12-22 09:21:41 +0330

    delete buttons added to the blogs for deleting each blog

--- src/BlogsList.jsx
+++ src/BlogsList.jsx
@@ -1,15 +1,16 @@
-const BlogsList = ({blogs, title}) => {
-  return (
+const BlogsList = ({ blogs, title, handleDelete }) => {
+	return (
 		<div className="blog-lists">
-      <h2>{title}</h2>
+			<h2>{title}</h2>
 			{blogs.map((blog) => (
 				<div className="blog-preview" key={blog.id}>
 					<h2>{blog.title}</h2>
-					<p>Written by :{blog.author}</p>
+					<p>Written by : {blog.author}</p>
+					<button onClick={()=>{handleDelete(blog.id)}}>Delete</button>
 				</div>
 			))}
 		</div>
-  );
-}
- 
+	);
+};
+
 export default BlogsList;
--- src/Home.jsx
+++ src/Home.jsx
@@ -5,12 +5,31 @@ const Home = () => {
 	let [blogs, setBlog] = useState([
 		{ title: "My new blog", body: "lorem ...", author: "alireza", id: 1 },
 		{ title: "Welcome Party!", body: "lorem ...", author: "abolfazl", id: 2 },
-		{ title: "Web dev top tips", body: "lorem ...", author: "farzad", id: 3 },
+		{
+			title: "Web dev top tips",
+			body: "lorem ...",
+			author: "alireza",
+			id: 3,
+		},
 	]);
+
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter((blog) => blog.id !== id);
+		setBlog(newBlogs);
+	};
 
 	return (
 		<div className="Home">
-			<BlogsList blogs={blogs} title="All Blogs!"/>
+			<BlogsList
+				blogs={blogs}
+				title="All Blogs!"
+				handleDelete={handleDelete}
+			/>
+			<BlogsList
+				blogs={blogs.filter((blog) => blog.author === "alireza")}
+				title="Alireza's blogs!"
+				handleDelete={handleDelete}
+			/>
 		</div>
 	);
 };
