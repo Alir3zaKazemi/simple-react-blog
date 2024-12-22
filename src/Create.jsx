560d6d0bafdd42dc402c76eb43cb3a00ef032754
Alir3za <alireza2212kazemi@gmail.com>
2024-12-22 22:15:21 +0330

    get form data of new blog creation and saved as object

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
 const Create = () => {
+	const [title, setTitle] = useState("");
+	const [body, setBody] = useState("");
+	const [author, setAuthor] = useState("alireza");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const blog = { title, body, author };
+		console.log(blog);
+	};
+
 	return (
 		<div className="create">
 			<h2>Add new blog</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>Blog title: </label>
-				<input type="text" required />
+				<input
+					type="text"
+					required
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
 				<label>Blog body: </label>
-				<textarea required></textarea>
+				<textarea
+					required
+					value={body}
+					onChange={(e) => setBody(e.target.value)}
+				></textarea>
 				<label>Blog author: </label>
-				<select>
+				<select value={author} onChange={(e) => setAuthor(e.target.value)}>
 					<option value="alireza">alireza</option>
 					<option value="abol">abol</option>
 				</select>
