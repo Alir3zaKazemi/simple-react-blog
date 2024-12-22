c70b132612de68e4e3f4b0660e908f2210a4deee
Alir3za <alireza2212kazemi@gmail.com>
2024-12-22 21:29:15 +0330

    reusing custom hook for seeing clicked blog

--- src/BlogsList.jsx
+++ src/BlogsList.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 const BlogsList = ({ blogs, title, handleDelete }) => {
 	return (
 		<div className="blog-lists">
 			<h2>{title}</h2>
 			{blogs.map((blog) => (
 				<div className="blog-preview" key={blog.id}>
-					<h2>{blog.title}</h2>
-					<p>Written by : {blog.author}</p>
-					<button
-						onClick={() => {
-							handleDelete(blog.id);
-						}}
-					>
-						Delete
-					</button>
+					<Link to={`/blogs/${blog.id}`}>
+						<h2>{blog.title}</h2>
+						<p>Written by : {blog.author}</p>
+						<button
+							onClick={() => {
+								handleDelete(blog.id);
+							}}
+						>
+							Delete
+						</button>
+					</Link>
 				</div>
 			))}
 		</div>
